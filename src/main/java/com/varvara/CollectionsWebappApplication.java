fdadb13289aefb5ac5b0df3f714e20082526359f
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-06-10 08:38:30 +0300

    Initial commit

---
+++ src/main/java/com/varvara/CollectionsWebappApplication.java
@@ -0,0 +1,13 @@
+package com.varvara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/varvara/CollectionsWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varvara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
