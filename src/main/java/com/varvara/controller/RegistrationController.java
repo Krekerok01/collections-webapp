6b74e71c8cae21bbe9e6f63a198a2139a030a144
Varvara Mamatsiuk <matyush2001@gmail.com>
2022-06-13 11:42:37 +0300

    Update registration button

---
+++ src/main/java/com/varvara/controller/RegistrationController.java
@@ -0,0 +1,57 @@
+package com.varvara.controller;
+
+
+import com.varvara.service.UserService;
+import com.varvara.user.CrmUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.logging.Logger;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+	
+    @Autowired
+    private UserService userService;
+	
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+	
+	@GetMapping("/registration")
+	public String showRegistrationPage(Model theModel) {
+		theModel.addAttribute("crmUser", new CrmUser());
+		return "registration";
+	}
+
+	@PostMapping("/processRegistrationForm")
+	public String processRegistrationForm(@Valid @ModelAttribute("crmUser") CrmUser theCrmUser, Model theModel,  BindingResult result) {
+
+		//???
+		if (result.hasErrors()){
+			System.out.println("ERR");
+			return "registration";
+		}
+
+		String userName = theCrmUser.getUsername();
+		logger.info("Processing registration form for: " + userName);
+
+		logger.info("theCrmUser: " + theCrmUser);
+
+
+        // create user account        						
+        userService.save(theCrmUser);
+
+        
+        logger.info("Successfully created user: " + userName);
+        
+        return "registration-confirmation";		
+	}
+}
